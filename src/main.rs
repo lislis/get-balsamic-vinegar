91b4a3184556adc56dc711e999b8567a84c86d61
lislis <mail@lislis.de>
2017-04-22 16:01:40 +0200

    player jumps

--- src/main.rs
+++ src/main.rs
@@ -18,23 +18,41 @@ struct Player {
     y: f64,
     w: f64,
     h: f64,
+    speed: f64,
     is_jumping: bool,
-    max_height: f64
+    max_height: f64,
+    ground_pos: f64
 }
 
 impl Player {
     pub fn new() -> Player {
         Player {
-            x: 50.0,
+            x: 60.0,
             y: 300.0,
             w: 40.0,
             h: 30.0,
+            speed: 2.0,
             is_jumping: false,
-            max_height: 200.0
+            max_height: 1700.0,
+            ground_pos: 300.0 // same as y at init
         }
     }
     pub fn update(&mut self, dt: f64) {
         println!("updating player");
+
+        if self.is_jumping {
+            self.y -= self.speed;
+            if self.y < self.max_height {
+                self.is_jumping = false;
+            }
+        }
+        if !self.is_jumping && self.y < self.ground_pos {
+            self.y += self.speed;
+        }
+
+    }
+    pub fn jump(&mut self) {
+        self.is_jumping = true;
     }
 }
 
@@ -172,7 +190,9 @@ fn main() {
                         }
                     }
                     "shop" => {
-
+                        if key == Key::Space {
+                            game.player.jump();
+                        }
                     }
                     _ => {}
 
@@ -206,7 +226,7 @@ fn main() {
                     "shop" => {
                         window.draw_2d(&e, |c, g| {
                             clear([1.0, 0.0, 0.0, 1.0], g);
-                            image(&player, c.transform, g);
+                            image(&player, c.transform.trans(game.player.x, game.player.y), g);
                         });
                     }
                     _ => {}
